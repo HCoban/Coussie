f52f97c09e29bc53c3d1b9cf15f3c350ad605bb8
Halil Coban <halil.coban@gmail.com>
2016-08-30 23:12:05 -0700

    removed login button

--- frontend/components/greeting.jsx
+++ frontend/components/greeting.jsx
@@ -4,6 +4,10 @@ export const Greeting = ({currentUser}) => {
   if (currentUser) {
     return <button>Log Out</button>;
   } else {
-    return <button>Log In</button>;
+    return (
+      <div>
+        <h1>Webpage under construction!</h1>
+      </div>
+    );
   }
 };
