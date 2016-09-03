3901a540202f7c56c6e7b5d1714a9f55b1fb1ce8
Halil Coban <halil.coban@gmail.com>
2016-09-02 19:17:40 -0700

    implemented restaurants to show single review

--- frontend/components/restaurant/show.jsx
+++ frontend/components/restaurant/show.jsx
@@ -9,27 +9,38 @@ class RestaurantShow extends React.Component {
   }
   render () {
     let restaurant = this.props.restaurant;
+    let reviews;
+    if (this.props.restaurant.reviews) {
+      reviews = <ReviewShow review={this.props.restaurant.review}/>;
+    } else {
+      reviews = "";
+    }
     return (
-      <div className="restaurant-show">
-        <div className="show-column-1">
-          <RestaurantMap restaurant={restaurant}/>
-          <div className="restaurant-info">
-            <h2>{this.props.restaurant.name}</h2>
-            <h3>{this.props.restaurant.address}</h3>
-            <h3>{this.props.restaurant.telephone}</h3>
+      <div className="restaurant-show-container">
+        <div className="restaurant-show">
+          <div className="show-column-1">
+            <RestaurantMap restaurant={restaurant}/>
+            <div className="restaurant-info">
+              <h2>{this.props.restaurant.name}</h2>
+              <h3>{this.props.restaurant.address}</h3>
+              <h3>{this.props.restaurant.telephone}</h3>
+            </div>
+          </div>
+          <div className="show-column-2">
+            <img src={this.props.restaurant.image_url}></img>
+            <img src={this.props.restaurant.image_url}></img>
+            <img src={this.props.restaurant.image_url}></img>
+            <img src={this.props.restaurant.image_url}></img>
+            <img src={this.props.restaurant.image_url}></img>
+            <img src={this.props.restaurant.image_url}></img>
+            <img src={this.props.restaurant.image_url}></img>
+            <img src={this.props.restaurant.image_url}></img>
           </div>
         </div>
-        <div className="show-column-2">
-          <img src={this.props.restaurant.image_url}></img>
-          <img src={this.props.restaurant.image_url}></img>
-          <img src={this.props.restaurant.image_url}></img>
-          <img src={this.props.restaurant.image_url}></img>
-          <img src={this.props.restaurant.image_url}></img>
-          <img src={this.props.restaurant.image_url}></img>
-          <img src={this.props.restaurant.image_url}></img>
-          <img src={this.props.restaurant.image_url}></img>
+        <div className="restaurant-review">
+          <span>Reviews</span>
+          {reviews}
         </div>
-        <ReviewShow review={this.props.restaurant.review}/>
       </div>
     );
   }
--- frontend/components/review/show.jsx
+++ frontend/components/review/show.jsx
@@ -6,15 +6,14 @@ const ReviewShow = ({review}) => (
       <ul className="reviwer-pic">
         <img src={review.reviewer.picture_url}></img>
       </ul>
-      <ul class="reviewer-details">
+      <ul className="reviewer-details">
         <li>{review.reviewer.username}</li>
         <li>{review.reviewer.city}</li>
       </ul>
     </div>
-    <div className="review-container">
-      <li className="rating">{review.vote}</li>
-      <li className="date">`Posted at: ${review.created_at.to_date}`</li>
-      <li className="description">{review.description}</li>
+    <div className="review-desc-container">
+      <li className="rating">{review.info.vote}</li>
+      <li className="description">{review.info.description}</li>
     </div>
   </div>
 );
