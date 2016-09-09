4b8084fae7697b680ab8cbff5255811680734073
Halil Coban <halil.coban@gmail.com>
2016-09-09 12:06:14 -0700

    unsplash credits added

--- frontend/components/credits/credits.jsx
+++ frontend/components/credits/credits.jsx
@@ -12,6 +12,34 @@ const Credits = () => (
     <li>Hamburger icon (Category: American) by David Lamm from the Noun Project</li>
     <li>Hot Dog icon (Category: German) by Mario Bieh from the Noun Project</li>
     <li>Noodles icon (Category: Asian) by kalamakara from the Noun Project</li>
+    <li>Restaurant picture by Serge Esteve from Unsplash project</li>
+    <li>Restaurant picture by Matt Jones from Unsplash project</li>
+    <li>Restaurant picture by Alex Jones from Unsplash project</li>
+    <li>Restaurant picture by Alva Pratt from Unsplash project</li>
+    <li>Restaurant picture by Andre Robillard from Unsplash project</li>
+    <li>Restaurant picture by Agnieszka P from Unsplash project</li>
+    <li>Restaurant picture by Henrique Felix from Unsplash project</li>
+    <li>Restaurant picture by Linh Nguyen from Unsplash project</li>
+    <li>Restaurant picture by Michael Browning from Unsplash project</li>
+    <li>Restaurant picture by Helen Martinez from Unsplash project</li>
+    <li>Restaurant picture by Thomas Vimare from Unsplash project</li>
+    <li>Restaurant picture by Kris Atomic from Unsplash project</li>
+    <li>Restaurant picture by Bonnie Meisels from Unsplash project</li>
+    <li>Restaurant picture by Loreta Pavoliene from Unsplash project</li>
+    <li>Restaurant picture by Kevin Curtis from Unsplash project</li>
+    <li>Restaurant picture by Brandon Morgan from Unsplash project</li>
+    <li>Restaurant picture by Claudia Viloria from Unsplash project</li>
+    <li>Restaurant picture by Joanna Boj from Unsplash project</li>
+    <li>Restaurant picture by Michael Browning from Unsplash project</li>
+    <li>Restaurant picture by Gabriel Gurrola from Unsplash project</li>
+    <li>Restaurant picture by Yutacar from Unsplash project</li>
+    <li>Restaurant picture by Tim Wright from Unsplash project</li>
+    <li>Restaurant picture by Ashim D'Silva from Unsplash project</li>
+    <li>Restaurant picture by Quentin Lagache from Unsplash project</li>
+    <li>Restaurant picture by Gerry Roarty from Unsplash project</li>
+    <li>Restaurant picture by Toronto Eaters from Unsplash project</li>
+    <li>Restaurant picture by Dan Gold from Unsplash project</li>
+    <li>Restaurant picture by Toronto Eaters from Unsplash project</li>
   </ul>
   </div>
 );
